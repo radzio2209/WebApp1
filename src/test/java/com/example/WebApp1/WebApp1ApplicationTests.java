20159c058a960dcef7a8759150c7471f6c0cfbec
Radoslaw <radoslaw.danielkiewicz@pollub.edu.pl>
2022-11-22 00:05:14 +0100

    Podstawowa aplikacja ze spring initalizer

---
+++ src/test/java/com/example/WebApp1/WebApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
